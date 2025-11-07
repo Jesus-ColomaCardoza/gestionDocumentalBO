f4088cd285e7653f00acde5124a159602a3d60ba
Jesus Coloma <colomacardoza16@gmail.com>
2025-11-07 17:08:01 -0500

    add sliders

--- src/modules/core/components/MainContainer.tsx
+++ src/modules/core/components/MainContainer.tsx
@@ -5,6 +5,11 @@ import { useEffect, useRef, useState } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../auth/context/AuthContext";
 import { Carousel } from "primereact/carousel";
+import slider1 from "./../../../assets/img/slider1.jpg"
+import slider2 from "./../../../assets/img/slider2.jpg"
+import slider3 from "./../../../assets/img/slider3.jpg"
+import slider4 from "./../../../assets/img/slider4.jpg"
+import slider5 from "./../../../assets/img/slider5.jpg"
 
 type MainContainerProps = {
   visible?: boolean;
@@ -53,11 +58,11 @@ const MainContainer = ({ visible }: MainContainerProps) => {
   const [images, setImages] = useState<string[]>([]);
 
   const sampleImages = [
-    "https://scontent.fpiu2-2.fna.fbcdn.net/v/t39.30808-6/552822438_812622635053847_584535605293799980_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGAfxqQfy3q_i6HNp-bwQJIvc5l_8VyyJK9zmX_xXLIkg582SfPfooexZWjHJHq3szW4gsoG8yZgC4ihM_5Hj0H&_nc_ohc=k-VQ8dZCHGwQ7kNvwEFD9Fx&_nc_oc=AdkFEZ5OqPRrNat8Sy0kuvKqJX1yMZ2xXSBbBtnnG2vcHQfh1qM3ZZWfLQC4gL9nSSo&_nc_zt=23&_nc_ht=scontent.fpiu2-2.fna&_nc_gid=WS2w8ya2BQLDsb8lDolkwg&oh=00_AfcU3BmxW2rEVxxfP2kxpYnmKUf4lPeNJwc0YSdFaZqz3g&oe=68FFA338",
-    "https://scontent.fpiu2-1.fna.fbcdn.net/v/t39.30808-6/542690515_795970536719057_8526782690322548958_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFjtis6Bgid38QFptlAeeItLnvJxAsYIswue8nECxgizEB__hR6Z4ZS2bsbKZ16NX0f35nXCrMEf197IhnR0yel&_nc_ohc=VOBPttuLXVoQ7kNvwEaIMJW&_nc_oc=Adknv2NA5HrhDenQB0IPKDirhnVPxP79rDtiJRHtWHjlokyCWY6hjEZWd3FKqClDbNA&_nc_zt=23&_nc_ht=scontent.fpiu2-1.fna&_nc_gid=jj7AwDEo3sw39mwpi-5Nsw&oh=00_AfcxVfP00NBI_Z1ikgkbdcd5ePRO0cTF3kOhrT83Q-hXpg&oe=68FFB2FF",
-    "https://scontent.fpiu2-2.fna.fbcdn.net/v/t39.30808-6/539553067_791333193849458_5278572874798904491_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFO8yYmDAXVeUeaK6bm_bTK-6IphbVvs437oimFtW-zjZgbwPFEZGvvmOXrRRWIOqhhTkVYOwBAMlNkvNYHTHp0&_nc_ohc=ynqOxUeDxs8Q7kNvwHpoL8S&_nc_oc=AdmIZUL-O4YfGjLvgpJYT065eLiX11GghM0WPX7xFnMJTf4g3TlTjzND9iolm0hTzg4&_nc_zt=23&_nc_ht=scontent.fpiu2-2.fna&_nc_gid=x5q-tAQe-TmdxjFJHnRoGg&oh=00_AfdzKndrPExULifziu_pfbOSmxbWhf1gu61YoXyE6TLssw&oe=68FFAE2F",
-    "https://scontent.fpiu2-1.fna.fbcdn.net/v/t39.30808-6/536281932_788533107462800_4764790020214456910_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeG76KV2WGKkGx6gcrsuTZ-qOu4t5_kfJPc67i3n-R8k97ywfO9x3hweaG7UYFkhRN2BBTEKXsf_N08KhllcRkyA&_nc_ohc=CAVi2O0uuMwQ7kNvwHGBhmT&_nc_oc=AdmaAIt3SmStT3YWZB_hWp2NDEfcm1eH3JRVd4NWsulUpPPq2_TpKFsIEbtbS_9W8Lw&_nc_zt=23&_nc_ht=scontent.fpiu2-1.fna&_nc_gid=_wcDx5b5Y8n3330NTkmWBg&oh=00_AfcdCGCkFYy7vVw2SwzU1ZnaDAqdpDmeMc0_buOVd2FtAQ&oe=68FF8D8F",
-    "https://scontent.fpiu2-1.fna.fbcdn.net/v/t39.30808-6/536481914_783452501304194_4992458065599789472_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFs46MhYyMR1tVOVEKHdGTf7B_Qy0HrUjTsH9DLQetSNIoIFSPPR3dNSSCLElt7s8zzD_slvWhhlz8T6ejQVPPy&_nc_ohc=l1kE7azd8jgQ7kNvwEASugr&_nc_oc=AdnI_vC3_EbPbPq5HLQmlFQg4_WYKPQ9KMH4Bxtvn65y7gJnbkFYHwpLft78zWg4bno&_nc_zt=23&_nc_ht=scontent.fpiu2-1.fna&_nc_gid=dMD3toTRiJO8Mqsp-WDEbA&oh=00_Afdf8FJQrlb48Wk9dV9GUqDvUzKBGTrrAW5gt-QOB0LJWQ&oe=68FF9F81",
+    slider1,
+    slider2,
+    slider3,
+    slider4,
+    slider5,
   ];
 
   useEffect(() => {
